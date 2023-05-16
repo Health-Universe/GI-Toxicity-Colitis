98cd1d31454705d18ecca9658eae689f19c52ca8
Dan Caron <dancaron@gmail.com>
2023-05-16 12:40:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import pandas as pd
 
+def calculate_irAEs_risk(age, sex, treatment_type, preexisting_conditions):
+    """
+    This function calculates the risk for Immune-Related Adverse Events (irAEs) for GI Toxicity - Colitis.
+    """
+    # The following is pseudo-code for calculating the score.
+    # Replace with the actual calculation algorithm.
+
+    risk = 0
+    risk += age
+    risk += sex
+    risk += treatment_type
+    risk += preexisting_conditions
+
+    return risk
+
+st.title('Immune-Related Adverse Events (irAEs) Risk Calculator for GI Toxicity - Colitis')
+st.write('This application is intended for medical professionals to calculate the risk for Immune-Related Adverse Events (irAEs) for GI Toxicity - Colitis. This score helps in predicting the risk of developing GI Toxicity - Colitis due to immune therapies.')
+
+age = st.slider('Age', 0, 100, 30)
+sex = st.selectbox('Sex', [(0, 'Male'), (1, 'Female')])
+treatment_type = st.selectbox('Type of Immune Therapy', [(0, 'CTLA-4 inhibitor'), (1, 'PD-1 inhibitor'), (2, 'PD-L1 inhibitor'), (3, 'Combination')])
+preexisting_conditions = st.selectbox('Preexisting GI Conditions', [(0, 'No'), (1, 'Yes')])
+
+if st.button('Calculate Risk'):
+    risk = calculate_irAEs_risk(age, sex[0], treatment_type[0], preexisting_conditions[0])
+    st.write('The irAEs risk for GI Toxicity - Colitis is: ', risk)
